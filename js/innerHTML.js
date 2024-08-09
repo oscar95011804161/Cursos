e308751f64f694aab17d8f658d46a5c22fbd4897
oscarvanegas <oscar_vanegas1@hotmail.com>
2024-08-09 12:22:57 -0500

    costura

--- js/innerHTML.js
+++ js/innerHTML.js
@@ -15,8 +15,9 @@ header.innerHTML =`
 	      		<ul class="navbar-nav ms-auto me-5 "> <!--ms-auto es para que mande el menu a la izquierda me-5 para correr un poco ala izquierda-->
 	      			
 		          	
-					<li class="nav-item"><a class="nav-link " aria-current="page" href="index.html">Inicio</a></li>
-					<li class="nav-item"><a class="nav-link " aria-current="page" href="disenografico.html">Curso de diseño grafico.</a></li>
+				<li class="nav-item"><a class="nav-link " aria-current="page" href="index.html">Inicio</a></li>
+				<li class="nav-item"><a class="nav-link " aria-current="page" href="disenografico.html">Curso de diseño grafico.</a></li>
+				<li class="nav-item"><a class="nav-link " aria-current="page" href="costura.html">Curso costura.</a></li>
 					
 		           
 		        </ul>
